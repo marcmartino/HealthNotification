82327442c64c027a7e78ee21abb901fe3b8966d4
Marc <mrmarcmartino@gmail.com>
2014-11-22 10:05:07 -0800

    moved folders and created header.html

---
+++ app/javascripts/app.js
@@ -0,0 +1,17 @@
+function myIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://api.hostip.info/get_json.php",false);
+    xmlhttp.send();
+
+    hostipInfo = JSON.parse(xmlhttp.responseText);
+
+    return hostipInfo;
+}
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save(myIP()).then(function(object) {
+  console.log("yay! it worked");
+});
